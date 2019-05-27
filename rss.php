933220baeaba556b8af5f93b20147eb63a9d2be4
Ham4690 <ham46903@gmail.com>
2019-05-27 19:57:01 +0900

    php test

--- rss.php
+++ rss.php
@@ -1,7 +1,7 @@
 <?php
     $rss = simplexml_load_file('http://feeds.feedburner.com/hatena/b/hotentry.css'); //取得先のRSSフィードを指定
-
-    echo "<pre>";
-    var_dump($rss);
-    echo "</pre>";
+    foreach($rss -> item as $item){
+        echo $item-> title;
+        echo '<br>';
+    }
 ?>
