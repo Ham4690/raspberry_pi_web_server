19372d0c430f35cc787fc3982a47bb6dd7aa5325
Ham4690 <ham46903@gmail.com>
2019-05-27 20:01:53 +0900

    fix the appearance of php

--- rss.php
+++ rss.php
@@ -1,7 +1,27 @@
-<?php
-    $rss = simplexml_load_file('http://feeds.feedburner.com/hatena/b/hotentry.css'); //取得先のRSSフィードを指定
-    foreach($rss -> item as $item){
-        echo $item-> title;
-        echo '<br>';
-    }
-?>
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RSS</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        .list-group-margin {
+            margin-top: 30px;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <p class="navbar-brand">RSS</p>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row"></div>
+    </div>
+</body>
+</html>
+<?php ?>
