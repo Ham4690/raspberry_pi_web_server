c9024541309952fc74298a41c8334b684a6dbddb
Ham4690 <ham46903@gmail.com>
2019-05-27 21:02:19 +0900

    comp

--- rss.php
+++ rss.php
@@ -21,30 +21,28 @@
 
     <div class="container">
         <div class="row">
-            <?php
-                createRssList("http://b.hatena.ne.jp/hotentry/it.rss");
-                createRssList("http://b.hatena.ne.jp/entrylist/it.rss");
-            ?>
+            <?php createRssList("http://feeds.feedburner.com/hatena/b/hotentry.css"); ?>
         </div>
     </div>
 </body>
 </html>
 <?php
 function createRssList(string $url) {
-    $count    = 0;
-    $count_mx = 10;
-    $rss      = simplexml_load_file($url);
+    $count    = 1;  // カウント保持用
+    $count_mx = 20; // 表示する行数
 
-    if ($rss === false) return false;
+    $rss = simplexml_load_file($url);   // RSSファイルを取得
+    if ($rss === false) return false;   // 取得失敗時、処理を終了
 
     echo("<div class=\"col-sm-12 col-md-6\">");
     echo("<div class=\"list-group list-group-margin\">");
     echo("<a href=\"" . $rss -> channel -> link  . "\" class=\"list-group-item active\">" . $rss -> channel -> title . "</a>");
 
+    // 記事一覧の処理
     foreach ($rss -> item as $item) {
-        if ($count >= $count_mx) break;
+        if ($count > $count_mx) break; // 最大数に達したら終了
 
-        echo("<a href=\"" . $item -> link . "\" class=\"list-group-item\">" . $item -> title . "</a>");
+        echo("<a href=\"" . $item -> link . "\" class=\"list-group-item\">".$count.":". $item -> title . "</a>");
         $count++;
     }
 
