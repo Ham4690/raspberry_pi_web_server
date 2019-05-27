51e22c86a1d460ebff2049a6197223d75f61bf7b
Ham4690 <ham46903@gmail.com>
2019-05-27 21:01:23 +0900

    comp

--- rss.php
+++ rss.php
@@ -31,21 +31,20 @@
 </html>
 <?php
 function createRssList(string $url) {
-    $count    = 0;  // カウント保持用
-    $count_mx = 10; // 表示する行数
+    $count    = 0;
+    $count_mx = 10;
+    $rss      = simplexml_load_file($url);
 
-    $rss = simplexml_load_file($url);   // RSSファイルを取得
-    if ($rss === false) return false;   // 取得失敗時、処理を終了
+    if ($rss === false) return false;
 
     echo("<div class=\"col-sm-12 col-md-6\">");
     echo("<div class=\"list-group list-group-margin\">");
     echo("<a href=\"" . $rss -> channel -> link  . "\" class=\"list-group-item active\">" . $rss -> channel -> title . "</a>");
 
-    // 記事一覧の処理
     foreach ($rss -> item as $item) {
-        if ($count >= $count_mx) break; // 最大数に達したら終了
+        if ($count >= $count_mx) break;
 
-        echo("<a href=\"" . $item -> link . "\" class=\"list-group-item\">". $item -> title . "</a>");
+        echo("<a href=\"" . $item -> link . "\" class=\"list-group-item\">" . $item -> title . "</a>");
         $count++;
     }
 
