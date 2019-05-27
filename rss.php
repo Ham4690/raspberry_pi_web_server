fe95e1f2cd4896ecc2d414b027d6be8e86f6b731
Ham4690 <ham46903@gmail.com>
2019-05-27 20:58:33 +0900

    comp

--- rss.php
+++ rss.php
@@ -32,7 +32,7 @@
 <?php
 function createRssList(string $url) {
     $count    = 1;  // カウント保持用
-    $count_mx = 20; // 表示する行数
+    $count_mx = 10; // 表示する行数
 
     $rss = simplexml_load_file($url);   // RSSファイルを取得
     if ($rss === false) return false;   // 取得失敗時、処理を終了
