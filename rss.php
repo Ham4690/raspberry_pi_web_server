c099ca4efa29771759c37eec69e457cc42871ed9
Ham4690 <ham46903@gmail.com>
2019-05-27 19:52:55 +0900

    get rss

--- rss.php
+++ rss.php
@@ -1,3 +1,7 @@
 <?php
-    phpinfo();
+    $rss = simplexml_load_file('http://feeds.feedburner.com/hatena/b/hotentry.css'); //取得先のRSSフィードを指定
+
+    echo "<pre>";
+    var_dump($rss);
+    echo "</pre>";
 ?>
