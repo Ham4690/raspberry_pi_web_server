b9f9178677cab5251aa8f3d42618a251d08b40dd
Ham4690 <ham46903@gmail.com>
2019-05-27 20:03:49 +0900

    add rss get prog

--- rss.php
+++ rss.php
@@ -24,4 +24,10 @@
     </div>
 </body>
 </html>
-<?php ?>
+<?php 
+$rss = simplexml_load_file('http://feeds.feedburner.com/hatena/b/hotentry.css'); //取得先のRSSフィードを指定
+foreach ($rss -> item as $item) {
+    echo $item -> title;
+    echo "<br>";
+}
+?>
